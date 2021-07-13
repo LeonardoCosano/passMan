21867f59d2d0059944f26f89fd4b98676b77d2ed
LeonardoCosano <leonardocosanocontact@gmail.com>
2021-07-13 18:21:13 +0200

    Add and search accounts working

---
+++ utils.py
@@ -0,0 +1,10 @@
+class colors:
+    BLACK  = '\33[30m'
+    RED    = '\33[31m'
+    BLUE   = '\33[34m'
+    GREEN  = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE   = '\33[96m'
+    VIOLET = '\33[35m'
+    DEFAULT = '\33[0m'
+    FAIL = '\033[91m'
